e7a57f82ca19abe1e3f072a5a02641d4421b36b0
Eduardo Palhares <xeduardopalhares@gmail.com>
2018-11-25 20:27:44 -0200

    Add Matriz.py

---
+++ Matrix/Matriz.py
@@ -0,0 +1,11 @@
+matriz = [
+    [10, 20, 30, 40],
+    [50, 60, 70, 80],
+    [90, 100, 110, 120]
+]
+
+print(matriz[1][2]) # valor 70
+
+vetor = [10, 20, 30, 40, 50]
+
+print(vetor[-1]) # útilmo elemento
