980d207612d29cf348ae6192260738e6321106a7
Eduardo Palhares <xeduardopalhares@gmail.com>
2023-07-01 13:43:07 -0300

    rearrange file folders

---
+++ array/matrix.py
@@ -0,0 +1,11 @@
+matrix = [
+    [10, 20, 30, 40],
+    [50, 60, 70, 80],
+    [90, 100, 110, 120]
+]
+
+print(matrix[1][2]) # valor 70
+
+vector = [10, 20, 30, 40, 50]
+
+print(vector[-1]) # útilmo elemento
